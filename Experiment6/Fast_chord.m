a5e0c39d0b08bc79a10178c30ac008667f777fdb
ianzhanghelicopter060323-tech <ianzhanghelicopter060323@gmail.com>
2026-05-30 23:12:00 +0800

    add loop time control(max loop time be no more than 20)

--- Experiment6/Fast_chord.m
+++ Experiment6/Fast_chord.m
@@ -6,12 +6,20 @@ function [X, k] = Fast_chord(f, x1, x2, e)
     % x1: one initial value
     % x2: two initial value
     
+    max_n = 20; % loop time control
+
     X(1) = x1;
     X(2)= x2;
-    k = 2;
+    k = 2; 
 
+    n=0;
     while abs(X(k) - X(k-1)) > e
+        if n >= max_n
+            disp('ERROR! Fst chord iteration FAILED because loop time reach limit');
+            return ;
+        end 
         X(k+1) = X(k) - (X(k) - X(k-1))*f(X(k))/(f(X(k)) - f(X(k-1)));
         k = k + 1;
+        n = n + 1;
     end
 end
